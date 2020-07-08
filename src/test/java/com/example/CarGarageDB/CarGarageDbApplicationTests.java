cb8184f2dc9d7b0e81c235c7fb20a9ad495511c7
Iulian <tintoiuandrei@yahoo.com>
2020-07-08 10:21:16 +0300

    TA:AM INCARCAT PROIECTUL,DAR AVEM O PROBLEMA CU JAR

---
+++ src/test/java/com/example/CarGarageDB/CarGarageDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarGarageDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGarageDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
