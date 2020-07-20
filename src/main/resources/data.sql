92e34c12baff566485aded847c13b385a01df9d8
Iulian <tintoiuandrei@yahoo.com>
2020-07-20 14:53:30 +0300

    TA:HOMEWORK 4 UPDATE

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 insert into t_cars (doors, platenumber, brand, rentingprice) values (4, 'BV-21-AVC', 'Audi', '13$/day');
+insert into t_cars (doors, platenumber, brand, rentingprice) values (4, 'BV-25-DSA', 'BMW', '15$/day');
+insert into t_cars (doors, platenumber, brand, rentingprice) values (4, 'BV-99-MAN', 'NISSAN', '15$/day');
